f6f58cea0df6035a57df24d5f30896bfee4c0da4
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-16 22:27:26 +0100

    Initial commit

---
+++ src/main/java/esercitazionesettimasettimana/EsercitazioneSettimaSettimanaApplication.java
@@ -0,0 +1,13 @@
+package esercitazionesettimasettimana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercitazioneSettimaSettimanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercitazioneSettimaSettimanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esercitazionesettimasettimana/EsercitazioneSettimaSettimanaApplicationTests.java
@@ -0,0 +1,13 @@
+package esercitazionesettimasettimana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercitazioneSettimaSettimanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
