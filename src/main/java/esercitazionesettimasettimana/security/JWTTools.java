4021a01395389641260ce32b4de3cebd23a448fa
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-17 11:12:54 +0100

    Added security configuration and filters

---
+++ src/main/java/esercitazionesettimasettimana/security/JWTTools.java
@@ -0,0 +1,36 @@
+package esercitazionesettimasettimana.security;
+
+import esercitazionesettimasettimana.enteties.User;
+import esercitazionesettimasettimana.exceptions.UnauthorizedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTTools {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+
+    public String createToken(User user) {
+        return Jwts.builder().setSubject(String.valueOf(user.getId()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
+    }
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Invalid token, please log in.");
+        }
+    }
+
+    public String extractIdFroToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parseClaimsJws(token).getBody().getSubject();
+    }
+}
